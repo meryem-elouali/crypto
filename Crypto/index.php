05f665025c721f84b321fec477502f5dcc77900f
Adamou Yahouza <yahouzaadamou99@gmail.com>
2023-03-08 00:50:38 +0100

    chiffrement

--- Crypto/index.php
+++ Crypto/index.php
@@ -11,20 +11,56 @@
   <div class="container">
     <div class="title">Merkle and Hellman algorithm</div>
     <div class="content">
-    <h3>Encrypt Form</h3>
-                    <form action="encrypt.php" method="post">
+    <h3>Keys Generation</h3>
+    <h5>Yahouza please enter your private key</h5>
+                    <form action="generate.php" method="post">
                         <div class="user-details">
                         <div class="input-box">
-                            <span class="details">Message clair M</span>
-                            <input type="text" name="message" placeholder="Enter your message " required>
-                        </div>
-                        <div class="input-box">
-                            <span class="details">Message chiffré C</span>
-                            <input type="text" name="cypher" placeholder="chiffertex "  disabled>
-                        </div>
+                        <span class="details">A supercroissant</span>
+                          <table>
+                            <tr>
+                              <td> 
+                            <input type="text" name="0"   required>
+                            </td>
+                            <td> 
+                            <input type="text" name="1"   required>
+                            </td>
+                            <td> 
+                            <input type="text" name="2"   required>
+                            </td>
+                            <td> 
+                            <input type="text" name="3"   required>
+                            </td>
+                            <td> 
+                            <input type="text" name="4"   required>
+                            </td>
+                            <td> 
+                            <input type="text" name="5"   required>
+                            </td>
+                            <td> 
+                            <input type="text" name="6"   required>
+                            </td>
+                            <td> 
+                            <input type="text" name="7"   required>
+                            </td>
+                          </tr>
+                          </table>
+                          <table>
+                            <tr>
+                              <td> 
+                            <span class="details">Entier Modulo U</span>
+                            <input type="text" name="u"   required>
+                            </td>
+                            <td> 
+                            <span class="details">Le multiplicateur premier P</span>
+                            <input type="text" name="p"   required>
+                            </td>
+                            </tr>
+                          </table>
                         </div>
                         <div class="button">
-                        <input type="submit" value="Encrypt">
+                        <input type="submit" value="Generate the public key">
+                        </div>
                         </div>
                 </form>
               </div> 
